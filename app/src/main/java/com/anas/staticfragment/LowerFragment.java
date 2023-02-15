a74ff1dca71f5307fc2fa4abdbf2dc2bd10c231e
anas2002 <anas4112002@gmail.com>
2023-02-15 23:17:03 +0530

    Error Free

---
+++ app/src/main/java/com/anas/staticfragment/LowerFragment.java
@@ -0,0 +1,28 @@
+package com.anas.staticfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class LowerFragment extends Fragment {
+
+    public LowerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_lower, container, false);
+        TextView txtLowerFrag=view.findViewById(R.id.txtLowerFrag);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/anas/staticfragment/UpperFragment.java
@@ -0,0 +1,27 @@
+package com.anas.staticfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class UpperFragment extends Fragment {
+
+    public UpperFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_upper, container, false);
+        TextView txtUpperFrag=view.findViewById(R.id.txtUpperFrag);
+        return view;
+    }
+}
